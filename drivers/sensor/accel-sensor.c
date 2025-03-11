7f129bc2adf25a6a42ee99cbb8c51df48f2f9897
Денис <baden.i.ua@gmail.com>
2025-03-11 03:00:59 +0200

    Вимірює кут.

--- drivers/sensor/accel-sensor.c
+++ drivers/sensor/accel-sensor.c
@@ -2,16 +2,114 @@
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/__assert.h>
 
 #include "accel-sensor.h"
+#include <math.h>
 
 LOG_MODULE_REGISTER(accel_sensor, LOG_LEVEL_DBG);
+
+#if !defined(M_PIf)
+#define M_PIf 3.1415927f
+#endif
+
+
+// Функція для обчислення довжини вектора
+float vector_length(_Vector3 v) {
+    return sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+// Функція для обчислення косинуса кута між двома векторами
+float angle_between_vectors(_Vector3 v1, _Vector3 v2) {
+    float dot_product = v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+    float magnitude_product = vector_length(v1) * vector_length(v2);
+    if (magnitude_product == 0) return 0.0f; // уникнення ділення на нуль
+    return acosf(dot_product / magnitude_product); // кут у радіанах
+}
+
+// Функція для обчислення кутів нахилу по осях
+void calculate_tilt_angles(_Vector3 ref, _Vector3 current, float *theta_x, float *theta_y, float *theta_z) {
+    *theta_x = atanf(current.y / current.z) - atanf(ref.y / ref.z);
+    *theta_y = atanf(current.x / current.z) - atanf(ref.x / ref.z);
+    *theta_z = atanf(current.x / current.y) - atanf(ref.x / ref.y);
+}
+
+int get_mma8652_val(const struct device *dev, struct sensor_value *val)
+{
+	if (sensor_sample_fetch(dev)) {
+		printk("Failed to fetch sample for device %s\n",
+		       dev->name);
+		return -1;
+	}
+
+	if (sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, &val[0])) {
+		return -1;
+	}
+
+	return 0;
+}
+
+
+static void adc_vbus_work_handler(struct k_work *work)
+{
+    struct k_work_delayable *delayable = k_work_delayable_from_work(work);
+    struct accel_sensor_data *data = CONTAINER_OF(delayable, struct accel_sensor_data, dwork);
+	const struct device *dev = data->accel_dev;
+
+	struct sensor_value val[3];
+
+    get_mma8652_val(dev, val);
+    float ax = sensor_value_to_double(&val[0]);
+    float ay = sensor_value_to_double(&val[1]);
+    float az = sensor_value_to_double(&val[2]);
+    _Vector3 current_acc = {ax, ay, az};
+    data->last_acc = current_acc;
+
+    // Обчислення загального кута нахилу
+    float theta = angle_between_vectors(data->ref_acc, current_acc);
+    // printk("Загальний кут нахилу: %.3f градусів\n", theta * (180.0f / M_PI));
+
+	LOG_DBG(
+		"Current values: X:%.4f Y:%.4f Z:%.4f"
+		"Tilt angle: %.4f"
+		, (double)ax, (double)ay, (double)az
+		, (double)theta
+	);
+
+	// TODO: Add callback
+	#if 0
+    	balance_overwrite_value((100.0f * theta * (180.0f / M_PIf)));
+	#endif
+    // force_send_state();
+
+    // LOG_ERR(
+    //     "Move sensor value X:%10.3f Y:%10.3f X:%10.3f",
+    //     sensor_value_to_double(&val[0]),
+    //     sensor_value_to_double(&val[1]),
+    //     sensor_value_to_double(&val[2])
+    //     // val[0].val1, val[1].val1, val[2].val1
+    // );
+    // adc_vbus_process();
+    k_work_schedule(&data->dwork, K_MSEC(data->sampling_period_ms));
+}
+
+
+// API
+static int _save_current_positoin_as_reference(const struct device *dev)
+{
+	struct accel_sensor_data *data = dev->data;
+	data->ref_acc = data->last_acc;
+	return 0;
+}
+
 
 static int init(const struct device *dev)
 {
 	const struct accel_sensor_config *cfg = dev->config;
+	struct accel_sensor_data *data = dev->data;
+
 	LOG_DBG("Initializing Accelerometer Sensor (%s)", dev->name);
 
 	const struct device *adev = cfg->accel_dev;
@@ -23,11 +121,6 @@ static int init(const struct device *dev)
 	LOG_DBG("Accelerometer device: %s is ready", adev->name);
 	// struct accel_sensor_data *data = dev->data;
 	int rc;
-
-	// if (!device_is_ready(cfg->bus.bus)) {
-	// 	LOG_ERR("Failed to get pointer to %s device!", cfg->bus.bus->name);
-	// 	return -ENODEV;
-	// }
 
     #if 0
 	rc = _set_sample_period(dev, data->sample_period_ms);
@@ -43,9 +136,11 @@ static int init(const struct device *dev)
 	}
     #endif
     
-	LOG_DBG("Starting periodic measurements");
-	rc = 0; /* scd30_write_register(dev, SCD30_CMD_START_PERIODIC_MEASUREMENT,
-				  SCD30_MEASUREMENT_DEF_AMBIENT_PRESSURE);*/
+	LOG_DBG("Starting periodic measurements (%d ms)", data->sampling_period_ms);
+
+	// TODO: Напевно треба перенести в макрос визначення змінної
+	k_work_init_delayable(&data->dwork, adc_vbus_work_handler);
+	k_work_schedule(&data->dwork, K_MSEC(data->sampling_period_ms));
 
 	return rc;
 }
@@ -66,6 +161,7 @@ static int _attr_set(const struct device *dev, enum sensor_channel chan,
 #endif
 
 static const struct accel_sensor_driver_api driver_api = {
+	.set_current_position_as_reference = _save_current_positoin_as_reference,
 	// .sample_fetch = scd30_sample_fetch,
 	// .channel_get = scd30_channel_get,
 	// .attr_get = _attr_get,
@@ -74,7 +170,8 @@ static const struct accel_sensor_driver_api driver_api = {
 
 #define ACCEL_SENSOR_DEFINE(inst)									    \
 	static struct accel_sensor_data data_##inst = {						\
-		.sampling_period_ms = DT_INST_PROP(inst, sampling_period_ms)		\
+		.sampling_period_ms = DT_INST_PROP(inst, sampling_period_ms),		\
+		.accel_dev = DEVICE_DT_GET(DT_PARENT(DT_DRV_INST(inst))), 			\
 	};											                        \
 	static const struct accel_sensor_config config_##inst = {			\
 		.accel_dev = DEVICE_DT_GET(DT_PARENT(DT_DRV_INST(inst))), 			\
