5be4a68c3b45df210e3bb2862c2b95300d19d01b
alexprokopov1992 <alexprokopov@gmail.com>
2025-11-03 06:23:55 +0200

    working

--- drivers/sensor/accel-sensor.c
+++ drivers/sensor/accel-sensor.c
@@ -1,7 +1,6 @@
 #define DT_DRV_COMPAT zephyr_accel_sensor
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/__assert.h>
@@ -9,8 +8,10 @@
 #include "accel-sensor.h"
 #include <math.h>
 
-// LOG_MODULE_REGISTER(accel_sensor, LOG_LEVEL_DBG);
-LOG_MODULE_REGISTER(accel_sensor, CONFIG_SENSOR_LOG_LEVEL);
+LOG_MODULE_REGISTER(accel_sensor, LOG_LEVEL_DBG);
+// LOG_MODULE_REGISTER(accel_sensor, CONFIG_SENSOR_LOG_LEVEL);
+
+K_THREAD_STACK_DEFINE(accel_thread_stack, ACCEL_THREAD_STACK_SIZE);
 
 #if !defined(M_PIf)
 #define M_PIf 3.1415927f
@@ -81,69 +82,6 @@ void calculate_tilt_angles(_Vector3 ref, _Vector3 current, float *theta_x, float
     *theta_y = atanf(current.x / current.z) - atanf(ref.x / ref.z);
     *theta_z = atanf(current.x / current.y) - atanf(ref.x / ref.y);
 }
-
-int get_mma8652_val(const struct device *dev, struct sensor_value *val)
-{
-	if (sensor_sample_fetch(dev)) {
-		printk("Failed to fetch sample for device %s\n",
-		       dev->name);
-		return -1;
-	}
-
-	if (sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, &val[0])) {
-		return -1;
-	}
-
-	return 0;
-}
-
-
-static int write_reg(const struct device *i2c_dev, uint8_t reg, uint8_t data) {
-    return i2c_reg_write_byte(i2c_dev, MMA8652_ADDR, reg, data);
-}
-
-static void mma8652fc_config_motion()
-{
-	const struct device *i2c_dev = device_get_binding("I2C_1");
-
-    if (!i2c_dev) {
-        LOG_ERR("Failed to get I2C device");
-        return;
-    }
-
-	uint8_t int_src;
-
-	i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, CTRL_REG1, &int_src);
-	int_src &= ~0x01;
-    write_reg(i2c_dev, CTRL_REG1, int_src);               // Standby
-
-    write_reg(i2c_dev, FF_MT_CFG, 0b11111000);         // Motion detection on XYZ
-	// write_reg(i2c_dev, F_SETUP, (0b10 << 6) | 16);               // Clear FF_MT_SRC register
-	write_reg(i2c_dev, F_SETUP, 0x00);               // Clear FF_MT_SRC register
-    write_reg(i2c_dev, FF_MT_THS, 0x01);               // ~0.063g
-    write_reg(i2c_dev, FF_MT_COUNT, 0x04);             // debounce count
-    write_reg(i2c_dev, CTRL_REG4, 0x04);               // Enable FF_MT interrupt
-    write_reg(i2c_dev, CTRL_REG5, 0x00);               // Route FF_MT to INT2
-
-	i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, CTRL_REG1, &int_src);
-	int_src |= 0x01;
-    write_reg(i2c_dev, CTRL_REG1, int_src);               // Active mode
-
-    k_msleep(10); // let it settle
-
-    i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, 0x0C, &int_src);
-    printk("INT_SOURCE: 0x%02X\n", int_src);
-
-	// i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, CTRL_REG5, &int_src);
-    // printk("Value is: 0x%02X\n", int_src);
-
-	// i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, F_SETUP, &int_src);
-    // printk("Value is: 0x%02X\n", int_src);
-
-	// i2c_reg_read_byte(i2c_dev, MMA8652_ADDR, FF_MT_COUNT, &int_src);
-    // printk("Value is: 0x%02X\n", int_src);
-}
-
 
 static void coarsering_tilt(struct accel_sensor_data *data, int level)
 {
@@ -357,252 +295,6 @@ static void process_move_mode(struct accel_sensor_data *data, const struct devic
 			data->summary_acc_move.z += current_acc.z;
 			data->samples_count_move++;
 		}
-}
-
-static void adc_vbus_work_handler_refactor(struct k_work *work)
-{
-	struct k_work_delayable *delayable = k_work_delayable_from_work(work);
-    struct accel_sensor_data *data = CONTAINER_OF(delayable, struct accel_sensor_data, dwork);
-	const struct device *dev = data->accel_dev;
-
-	struct sensor_value val[3];
-    get_mma8652_val(dev, val);
-
-	 _Vector3 current_acc = {
-        sensor_value_to_double(&val[0]),
-        sensor_value_to_double(&val[1]),
-        sensor_value_to_double(&val[2])
-    };
-    data->last_acc_tilt = current_acc;
-
-	int64_t current_time = k_uptime_get();
-
-	if (both_mode_disarmed(data))
-	{
-		process_disarmed_move(data, dev, current_acc, current_time);
-		k_work_schedule(&data->dwork, K_MSEC(MOVE_SENSOR_SAMPLE_TIME));
-		return;
-	}
-
-	bool allowed = false;
-	if (data->mode_move == ACCEL_SENSOR_MODE_ARMED) {
-		data->skip_counter++;
-		if (data->skip_counter > 9) {
-			data->skip_counter = 0;
-			allowed = true;
-		} else {
-			allowed = false;
-		}
-	} else {
-		allowed = true;
-	}
-
-	if (data->mode_tilt == ACCEL_SENSOR_MODE_ARMED && allowed)
-	{
-		if (!process_tilt_mode(data, dev, current_acc, current_time)) return;
-	}
-
-	if (data->mode_move == ACCEL_SENSOR_MODE_ARMED || data->mode_move == ACCEL_SENSOR_MODE_ALARM){
-		process_move_mode(data, dev, current_acc, current_time);
-	}
-
-	k_work_schedule(&data->dwork, K_MSEC(data->sampling_period_ms));
-}
-
-static void adc_vbus_work_handler(struct k_work *work)
-{
-    struct k_work_delayable *delayable = k_work_delayable_from_work(work);
-    struct accel_sensor_data *data = CONTAINER_OF(delayable, struct accel_sensor_data, dwork);
-
-	const struct device *dev = data->accel_dev;
-
-	struct sensor_value val[3];
-
-    get_mma8652_val(dev, val);
-    float ax = sensor_value_to_double(&val[0]);
-    float ay = sensor_value_to_double(&val[1]);
-    float az = sensor_value_to_double(&val[2]);
-    _Vector3 current_acc = {ax, ay, az};
-    data->last_acc_tilt = current_acc;
-
-	if (data->mode_tilt == ACCEL_SENSOR_MODE_DISARMED && data->mode_move == ACCEL_SENSOR_MODE_DISARMED) {
-		int64_t current_time = k_uptime_get();
-		if (data->samples_count_move_disarmed >= MOVE_SENSOR_SAMPLE_COUNT){
-			data->last_acc_move_disarmed.x = data->summary_acc_move_disarmed.x / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->last_acc_move_disarmed.y = data->summary_acc_move_disarmed.y / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->last_acc_move_disarmed.z = data->summary_acc_move_disarmed.z / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->summary_acc_move_disarmed.x = 0;
-			data->summary_acc_move_disarmed.y = 0;
-			data->summary_acc_move_disarmed.z = 0;
-			data->samples_count_move_disarmed = 0;
-
-			float acc_len = vector_length(data->last_acc_move_disarmed);
-			_Vector3 accelerate = {data->last_acc_move_disarmed.x - data->ref_acc_move_disarmed.x, data->last_acc_move_disarmed.y - data->ref_acc_move_disarmed.y, data->last_acc_move_disarmed.z - data->ref_acc_move_disarmed.z};
-			float accel = vector_length(accelerate);
-
-			if (data->gravity_disarmed > 0)
-			{
-				float change = (data->gravity_disarmed - acc_len)/data->gravity_disarmed;
-				if (change < border_move && change > -border_move)
-				{
-					data->gravity_disarmed = acc_len;
-					data->ref_acc_move_disarmed = data->last_acc_move_disarmed;
-				} else {
-					if (accel > data->warn_zone_move[data->selected_warn_zone_move]*data->gravity_disarmed) {
-						if (current_time - data->last_trigger_time_disarmed_move > MIN_WARN_INTERVAL) {
-							LOG_DBG("Disarmed move triggered");
-							LOG_DBG("Move sensor value X:%10.6f Y:%10.6f Z:%10.6f accel: %10.6f gravity: %10.6f last_acc: %10.6f", (double)data->last_acc_move_disarmed.x, (double)data->last_acc_move_disarmed.y, (double)data->last_acc_move_disarmed.z, (double)accel, (double)data->gravity_disarmed, (double)acc_len);
-							data->last_trigger_time_disarmed_move = current_time;
-							data->disarm_move_handler(dev, data->disarm_move_trigger);
-						}
-					}
-				}
-			}
-
-		} else {
-			data->summary_acc_move_disarmed.x += ax;
-			data->summary_acc_move_disarmed.y += ay;
-			data->summary_acc_move_disarmed.z += az;
-			data->samples_count_move_disarmed++;
-		}
-		k_work_schedule(&data->dwork, K_MSEC(MOVE_SENSOR_SAMPLE_TIME));
-		return;
-	}
-
-	bool allowed = false;
-	if (data->mode_move == ACCEL_SENSOR_MODE_ARMED) {
-		data->skip_counter++;
-		if (data->skip_counter > 9) {
-			data->skip_counter = 0;
-			allowed = true;
-		} else {
-			allowed = false;
-		}
-	} else {
-		allowed = true;
-	}
-
-	if (data->mode_tilt == ACCEL_SENSOR_MODE_ARMED && allowed)
-	{
-		float pow_cos_theta = cospow2_between_vectors(data->ref_acc_tilt, current_acc);
-		if (pow_cos_theta == 0) {
-			k_work_schedule(&data->dwork, K_MSEC(data->sampling_period_ms));
-			return;
-		}
-
-		int64_t current_time = k_uptime_get();
-
-		if (pow_cos_theta < data->main_zone_cos_pow2[data->current_main_zone_tilt] && data->main_zone_active_tilt)
-		{
-			if (!data->max_main_alert_level_tilt){
-				data->mode_tilt = ACCEL_SENSOR_MODE_ALARM;
-				data->in_warn_alert_tilt = true;
-				data->in_main_alert_tilt = true;
-				coarsering_tilt(data, 1);
-				data->last_trigger_time_main_tilt = current_time;
-				LOG_DBG("MAIN TRIGGER TILT");
-				data->main_handler_tilt(dev, data->main_trigger_tilt);
-			}
-			k_timer_start(&data->increase_sensivity_timer_tilt, K_SECONDS(INCREASE_SENSIVITY_TIME), K_NO_WAIT);
-		} else if (pow_cos_theta < data->warn_zone_cos_pow2[data->current_warn_zone_tilt] && data->warn_zone_active_tilt) {
-			if (!data->max_warn_alert_level_tilt) {
-				if (current_time - data->last_trigger_time_warn_tilt > MIN_WARN_INTERVAL) {
-					data->in_warn_alert_tilt = true;
-					coarsering_tilt(data, 0);
-					data->last_trigger_time_warn_tilt = current_time;
-					LOG_DBG("WARN TRIGGER TILT");
-					data->warn_handler_tilt(dev, data->warn_trigger_tilt);
-				}
-				k_timer_start(&data->increase_sensivity_timer_tilt, K_SECONDS(INCREASE_SENSIVITY_TIME), K_NO_WAIT);
-			}	
-		}
-		// LOG_DBG(
-		// 	"Current values: X:%.4f Y:%.4f Z:%.4f"
-		// 	" pow cos theta: %.8f"
-		// 	, (double)ax, (double)ay, (double)az
-		// 	, (double)(pow_cos_theta)
-		// );
-	}
-
-	if (data->mode_move == ACCEL_SENSOR_MODE_ARMED || data->mode_move == ACCEL_SENSOR_MODE_ALARM){
-
-		int64_t current_time = k_uptime_get();
-		if (data->samples_count_move >= MOVE_SENSOR_SAMPLE_COUNT){
-			data->last_acc_move.x = data->summary_acc_move.x / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->last_acc_move.y = data->summary_acc_move.y / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->last_acc_move.z = data->summary_acc_move.z / (float)MOVE_SENSOR_SAMPLE_COUNT;
-			data->summary_acc_move.x = 0;
-			data->summary_acc_move.y = 0;
-			data->summary_acc_move.z = 0;
-			data->samples_count_move = 0;
-
-			if (data->mode_move == ACCEL_SENSOR_MODE_ARMED)
-			{
-				float acc_len = vector_length(data->last_acc_move);
-				_Vector3 accelerate = {data->last_acc_move.x - data->ref_acc_move.x, data->last_acc_move.y - data->ref_acc_move.y, data->last_acc_move.z - data->ref_acc_move.z};
-				float accel = vector_length(accelerate);
-				if (data->gravity > 0)
-				{
-					float change = (data->gravity - acc_len)/data->gravity;
-					if (change < border_move && change > -border_move)
-					{
-						data->gravity = acc_len;
-						data->ref_acc_move = data->last_acc_move;
-					} else {
-						if (accel > data->main_zone_move[data->current_main_zone_move]*data->gravity && data->main_zone_active_move)
-						{
-							if (!data->max_main_alert_level_move){
-								data->mode_move = ACCEL_SENSOR_MODE_ALARM;
-								coarsering_move(data, 1);
-								LOG_DBG("Move Main zone move triggered");
-								data->last_trigger_time_main_move = current_time;
-								data->main_handler_move(dev, data->main_trigger_move);
-							}
-							k_timer_start(&data->increase_sensivity_main_timer_move, K_SECONDS(INCREASE_SENSIVITY_TIME), K_NO_WAIT);
-						} else {
-							if (accel > data->warn_zone_move[data->current_warn_zone_move]*data->gravity && data->warn_zone_active_move) {
-								if (!data->max_warn_alert_level_move) {
-									if (current_time - data->last_trigger_time_warn_move > MIN_WARN_INTERVAL) {
-										coarsering_move(data, 0);
-										LOG_DBG("Move Warn zone move triggered");
-										data->last_trigger_time_warn_move = current_time;
-										data->warn_handler_move(dev, data->warn_trigger_move);
-									}
-								}
-								k_timer_start(&data->increase_sensivity_warn_timer_move, K_SECONDS(INCREASE_SENSIVITY_TIME), K_NO_WAIT);
-							}
-						}
-					}
-
-					// LOG_DBG("Move sensor value X:%10.6f Y:%10.6f Z:%10.6f accel: %10.6f gravity: %10.6f last_acc: %10.6f", (double)data->last_acc_move.x, (double)data->last_acc_move.y, (double)data->last_acc_move.z, (double)accel, (double)data->gravity, (double)acc_len);
-				}
-			}	
-		} else {
-			data->summary_acc_move.x += ax;
-			data->summary_acc_move.y += ay;
-			data->summary_acc_move.z += az;
-			data->samples_count_move++;
-		}
-	}
-
-	// TODO: Add callback
-	#if 0
-    	balance_overwrite_value((100.0f * theta * (180.0f / M_PIf)));
-	#endif
-    // force_send_state();
-
-    // LOG_ERR(
-    //     "Move sensor value X:%10.3f Y:%10.3f X:%10.3f",
-    //     sensor_value_to_double(&val[0]),
-    //     sensor_value_to_double(&val[1]),
-    //     sensor_value_to_double(&val[2])
-    //     // val[0].val1, val[1].val1, val[2].val1
-    // );
-    // adc_vbus_process();
-
-
-
-    k_work_schedule(&data->dwork, K_MSEC(data->sampling_period_ms));
 }
 
 static void init_warn_zones_tilt(const struct device *dev)
@@ -914,11 +606,11 @@ static void increase_sensivity_main_timer_handler_move(struct k_timer *timer)
 	}
 }
 
-static void trigger_handler(const struct device *dev, const struct sensor_trigger *trigger)
+static void trigger_handler(const struct device *adev, const struct sensor_trigger *trigger)
 {
 	ARG_UNUSED(trigger);
 
-	if (sensor_sample_fetch(dev)) {
+	if (sensor_sample_fetch(adev)) {
 		printf("sensor_sample_fetch failed\n");
 		return;
 	}
@@ -926,17 +618,20 @@ static void trigger_handler(const struct device *dev, const struct sensor_trigge
 	k_sem_give(&sema);
 }
 
+
 void accel_thread(void *dev_ptr, void *arg2, void *arg3)
 {
 	const struct device *dev = (const struct device *)dev_ptr;
 	struct sensor_value data_val[3];
 	struct accel_sensor_data *data = dev->data;
+	const struct accel_sensor_config *cfg = dev->config;
+	const struct device *adev = cfg->accel_dev;
 	LOG_INF("Accel thread started (%s)", dev->name);
 
 	while (data->thread_running) {
 		k_sem_take(&sema, K_FOREVER);
 
-		if (sensor_channel_get(dev, SENSOR_CHAN_ACCEL_XYZ, data_val) < 0) {
+		if (sensor_channel_get(adev, SENSOR_CHAN_ACCEL_XYZ, data_val) < 0) {
 			printf("sensor_channel_get failed\n");
 			continue;
 		}
@@ -1134,7 +829,7 @@ static int init(const struct device *dev)
 	
 	LOG_DBG("Accelerometer device: %s is ready", adev->name);
 
-	if (sensor_trigger_set(dev, &trig, trigger_handler)) {
+	if (sensor_trigger_set(adev, &trig, trigger_handler)) {
 		LOG_ERR("Could not set trigger");
 		return -ENODEV;
 	}
@@ -1577,18 +1272,18 @@ static const struct accel_sensor_driver_api driver_api = {
 	// .attr_set = _attr_set,
 };
 
-#define ACCEL_SENSOR_DEFINE(inst)									    \
-	static struct accel_sensor_data data_##inst = {						\
-		.sampling_period_ms = DT_INST_PROP(inst, sampling_period_ms),		\
-		.accel_dev = DEVICE_DT_GET(DT_PARENT(DT_DRV_INST(inst))), 			\
-	};											                        \
-	static const struct accel_sensor_config config_##inst = {			\
-		.accel_dev = DEVICE_DT_GET(DT_PARENT(DT_DRV_INST(inst))), 			\
-	};											                        \
-												                        \
-	DEVICE_DT_INST_DEFINE(inst, init, NULL, &data_##inst, &config_##inst,	\
-			      POST_KERNEL, CONFIG_ACCEL_SENSOR_INIT_PRIORITY, &driver_api);
-
-// CONFIG_KERNEL_INIT_PRIORITY_DEVICE
+#define ACCEL_SENSOR_DEFINE(inst)                                       \
+    static struct accel_sensor_data data_##inst = {                     \
+        .sampling_period_ms = DT_INST_PROP(inst, sampling_period_ms),   \
+        .accel_dev = DEVICE_DT_GET(DT_INST_PHANDLE(inst, accel_device)),\
+    };                                                                   \
+    static const struct accel_sensor_config config_##inst = {           \
+        .accel_dev = DEVICE_DT_GET(DT_INST_PHANDLE(inst, accel_device)),\
+    };                                                                   \
+                                                                         \
+    DEVICE_DT_INST_DEFINE(inst, init, NULL,                \
+                          &data_##inst, &config_##inst,                 \
+                          POST_KERNEL, CONFIG_ACCEL_SENSOR_INIT_PRIORITY,\
+                          &driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(ACCEL_SENSOR_DEFINE);
--- drivers/sensor/accel-sensor.h
+++ drivers/sensor/accel-sensor.h
@@ -2,18 +2,6 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/i2c.h>
-
-#define MMA8652_ADDR 0x1D
-#define CTRL_REG1      0x2A
-#define FF_MT_CFG      0x15
-#define FF_MT_THS      0x17
-#define FF_MT_COUNT    0x18
-#define CTRL_REG4      0x2D
-#define CTRL_REG5      0x2E
-#define F_SETUP        0x09
-
-
 
 enum accel_sensor_mode {
     ACCEL_SENSOR_MODE_ARMED=0,
@@ -46,7 +34,6 @@ enum accel_sensor_channel {
 };
 
 struct accel_sensor_config {
-	// const struct i2c_dt_spec bus;
 	const struct device *accel_dev;
 };
 
